4e163bc6fe0d117656afbb054f6c51a14297ace4
MylavaramSaei <mylavaramnagasaei@gmail.com>
2025-06-30 18:23:19 +0530

    Time: 1 ms (92.41%), Space: 17 MB (18.81%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int largestRectangleArea(int* heights, int heightsSize) {
+    int* stack = (int*)malloc(sizeof(int) * (heightsSize + 1));
+    int top = -1;
+    int maxArea = 0;
+
+    for (int i = 0; i <= heightsSize; i++) {
+        int currHeight = (i == heightsSize) ? 0 : heights[i];
+
+        while (top != -1 && heights[stack[top]] > currHeight) {
+            int height = heights[stack[top--]];
+            int width = (top == -1) ? i : i - stack[top] - 1;
+            int area = height * width;
+            if (area > maxArea) maxArea = area;
+        }
+
+        stack[++top] = i;
+    }
+
+    free(stack);
+    return maxArea;
+}
